7d83b59d868a685333adf5e62d2f9b5200b08d60
Anibal Mendoza <anibalmd32@gmail.com>
2024-04-14 12:17:12 -0400

    add swagger enviroment

---
+++ pf_app/src/swagger.ts
@@ -0,0 +1,37 @@
+import swaggerJSDoc from "swagger-jsdoc"
+import swaggerUi from "swagger-ui-express"
+
+// Options for the swagger docs
+const options: swaggerJSDoc.Options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "PayFlow API",
+			version: "1.0.0",
+			description: "PayFlow API",
+		},
+	},
+	apis: ["./src/**/*.ts"],
+}
+
+// Documentation in JSON format
+const swaggerSpec = swaggerJSDoc(options)
+
+// Function to setup and return swagger-ui-express middleware
+const swaggerUiOptions = {
+	customCss: ".swagger-ui .topbar { display: none }",
+	customSiteTitle: "PayFlow | API",
+	customfavIcon: "/favicon.svg",
+}
+
+/**
+ * Sets up and returns the swagger-ui-express middleware.
+ *
+ * @param {any} app - the Express application
+ * @return {void}
+ */
+export const swaggerDocs = (app: any) => {
+	app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerUiOptions))
+}
+
+export default swaggerDocs
